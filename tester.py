e271da3a1b9ce577557fa1015e827e79f4520120
batunii <sy.soni@outlook.com>
2026-05-11 18:04:20 +0100

    Tester
    II have added 2 files :
    1. `tester.py` and `config.yaml`
    2. We are saving `config.yaml` usiing `tester.py` and then loading its contents

---
+++ tester.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from typing import Optional
+from ET_has import InitET, Blobber, Commiter
+
+init = InitET()
+blobber = Blobber(init)
+commiter = Commiter(init, blobber)
+
+commiter.make_commit(
+    tracked_files=[Path("config.yaml")],
+    author="app",
+    message="autosave"
+)
+last_commit: Optional[dict] = commiter.check_last_commit("config.yaml")
+if last_commit:
+    blob_id = last_commit["files"]["config.yaml"]
+    print(blob_id)
+    file_content: Optional[bytes] = blobber.load_blob(blob_hash=blob_id)
+    print(file_content if file_content else "Nothing")
